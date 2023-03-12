e0202749c9ec5f3446a71cbcfcec07ded1c03e06
Halfgork <gorkemsakalak@gmail.com>
2023-03-13 01:47:21 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# El-Calabozo
+
+## General Information
+
+- I learned how to build a dungeon theme game on Solana in this project. I covered the
+  on-chain program's code using the Seahorse framework with the Python programming language.
+  Thirdweb makes it easier for us the NFT collection drop,
+  the mint, and the program code. Loaded, created, and used game assets with Kaboom.
---
+++ src/utils/constants.ts
@@ -0,0 +1,15 @@
+import { PublicKey } from "@solana/web3.js";
+
+/** Kaboom */
+export const OLDMAN = "oldman";
+export const OLDMAN2 = "oldman2";
+export const OLDMAN3 = "oldman3";
+export const SPEED = 120;
+export const HERO = "hero2";
+export const SWORD = "sword2";
+
+/** Solana */
+export const PROGRAM_ID = new PublicKey("");
+export const TW_COLLECTION_ADDRESS =""
+import.meta.env.VITE_TW_COLLECTION_ADDRESS || "";
+export const NETWORK_URL = "";
